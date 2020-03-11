972fed295a0708dd6df50a73334d12aec2d22afb
Diego <diegocontacto1996@gmail.com>
2020-03-11 17:49:01 -0300

    Segundo Commit Para ENTRENADOR ordenado

--- ENTRENADOR/entrenador_def_modelo.py
+++ ENTRENADOR/entrenador_def_modelo.py
@@ -5,19 +5,17 @@ from pyAudioAnalysis import audioAnalysis
 import matplotlib.pyplot as plt
 from pyAudioAnalysis import audioTrainTest as aT
 
+
+#SUPPORT VECTOR MACHINE
 #aT.featureAndTrain(["SELECCION/AGRESIVOS/","SELECCION/NO_AGRESIVOS/"], 1.0, 0.10, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSentic", False)
 #aT.featureAndTrain(["SELECCION/AGRESIVOS/","SELECCION/NO_AGRESIVOS/"], 1.0, 0.10, 0.020, 0.020, "svm", "svmSentimiento", False)
 
+#GRADIENT BOOSTING
 aT.featureAndTrain(["SELECCION/AGRESIVOS/","SELECCION/NO_AGRESIVOS/"], 1.0, 0.10, 0.020, 0.020, "gradientboosting", "gbSentic", False)
 
 #aT.featureAndTrain(["SELECCION/CHILE/AGRESIVOS/","SELECCION/CHILE/NO_AGRESIVOS/"], 1.0, 0.10, 0.020, 0.020, "gradientboosting", "gbSenticChile", False)
 
 #aT.featureAndTrain(["SELECCION/SAVEE/AGRESIVOS/","SELECCION/SAVEE/NO_AGRESIVOS/"], 1.0, 0.10, 0.020, 0.020, "gradientboosting", "gbSenticSavee", False)
-
-
-
-#print(aT.fileClassification("68.wav", "svmSentic","svm"))
-
 
 
 #RANDOM FOREST
@@ -28,15 +26,4 @@ aT.featureAndTrain(["SELECCION/AGRESIVOS/","SELECCION/NO_AGRESIVOS/"], 1.0, 0.10
 #aT.featureAndTrain(["SELECCION/SAVEE/AGRESIVOS/","SELECCION/SAVEE/NO_AGRESIVOS/"], 1.0, 0.10, 0.020, 0.020, "randomforest", "gbSenticSavee", False)
 
 
-
-
-"""resultado=aT.fileClassification("voz.wav", "gbSentic","gradientboosting")
-
-if resultado[0]==0.0:
-	sentimiento="Agresivo"
-else:
-	sentimiento="No Agresivo"
-
-print(sentimiento)
-print(resultado)"""
 
